5077698a30f047af52e4c8ac629c0a442b8f11cc
Rochan543 <premrochan@gmail.com>
2026-02-09 20:21:09 +0530

    new latest backend deploy

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,12 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import pg from "pg";
+import { Pool } from "pg";
 import * as schema from "../shared/schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const pool = new pg.Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
